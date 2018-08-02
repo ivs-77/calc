52536008289736ca962eb0dd131dfbe730e2ef8d
ivs-77 <ivs_77@mail.ru>
2018-08-02 20:52:46 +0700

    v32

--- test/reset_data.sh
+++ test/reset_data.sh
@@ -3,8 +3,8 @@
 printf "stop\n"|nc localhost 1500
 printf "\n"
 ps ax|grep calc
-PGPASSWORD=calc psql -U calc -f ./../sql/clear_data.sql
-PGPASSWORD=calc psql -U calc -f ./../sql/insert_sample_data.sql
+PGPASSWORD=calc psql -U calc -h 127.0.0.1 -f ./../sql/clear_data.sql
+PGPASSWORD=calc psql -U calc -h 127.0.0.1 -f ./../sql/insert_sample_data.sql
 cd ../bin
 ./calc
 ps ax|grep calc
